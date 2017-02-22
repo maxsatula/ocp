bb4d21c6c218ca594af34f42faea0b1816e28095
Max Satula <maksym.satula@gmail.com>
2017-02-21 23:27:13 -0500

    Bumped to version 0.2

--- win/include/config.h
+++ win/include/config.h
@@ -19,7 +19,7 @@
 #undef PACKAGE_TARNAME
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.1"
+#define PACKAGE_VERSION "0.2"
 
 /* Version number of package */
-#define VERSION "0.1"
+#define VERSION "0.2"
