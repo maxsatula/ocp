0f22e492ff84ab477058a5294914817af9c96c24
Max Satula <maksym.satula@gmail.com>
2019-01-11 17:43:51 -0500

    When testing `--ls`, install supporting objects first

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 PATH=$PATH:$PWD/src
 CON=ocptest/test@localhost:1521/XEPDB1
 
@@ -7,5 +7,6 @@ ocp $CON DATA_PUMP_DIR:somefile.dmp localfile.dmp
 ocp $CON -9 DATA_PUMP_DIR:somefile.dmp onthefly.dmp
 ocp $CON -9 onthefly.dmp DATA_PUMP_DIR:
 ocp $CON --gzip DATA_PUMP_DIR:somefile.dmp
+ocp $CON --install
 ocp $CON --ls DATA_PUMP_DIR
 ocp $CON --rm DATA_PUMP_DIR:onthefly.dmp
