d2cd2852a5eb8086f1f5665c0b2e87dfe7c12af7
Max Satula <maksym.satula@gmail.com>
2019-01-11 17:17:11 -0500

    Oracle XE 18c supports Java, now can test `--ls` option

--- .travis/oracle/customize.sh
+++ .travis/oracle/customize.sh
@@ -20,6 +20,17 @@ GRANT EXECUTE
 GRANT SELECT
    ON v_\$session
    TO ocptest;
+
+BEGIN
+  FOR d IN (SELECT directory_path
+              FROM dba_directories
+             WHERE directory_name = 'DATA_PUMP_DIR') LOOP
+    dbms_java.grant_permission( 'OCPTEST', 'SYS:java.io.FilePermission', d.directory_path, 'read' );
+    dbms_java.grant_permission( 'OCPTEST', 'SYS:java.io.FilePermission', d.directory_path || '/*', 'read' );
+  END LOOP;
+END;
+/
+
 SQL
 
 expdp system/$ORACLE_PWD@127.0.0.1:1521/XEPDB1 logfile=expdp_system.log dumpfile=somefile.dmp directory=data_pump_dir
--- .travis/test.sh
+++ .travis/test.sh
@@ -2,11 +2,10 @@
 PATH=$PATH:$PWD/src
 CON=ocptest/test@localhost:1521/XEPDB1
 
-# Oracle XE does not support Java, thus no tests for --ls
-
 ocp $CON --list-directories
 ocp $CON DATA_PUMP_DIR:somefile.dmp localfile.dmp
 ocp $CON -9 DATA_PUMP_DIR:somefile.dmp onthefly.dmp
 ocp $CON -9 onthefly.dmp DATA_PUMP_DIR:
 ocp $CON --gzip DATA_PUMP_DIR:somefile.dmp
+ocp $CON --ls DATA_PUMP_DIR
 ocp $CON --rm DATA_PUMP_DIR:onthefly.dmp
